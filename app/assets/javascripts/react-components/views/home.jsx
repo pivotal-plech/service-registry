d6693198af40ac2242342a79b3d21cd918a5f0b1
Spencer Hurst <pair+spencer@pivotallabs.com>
2015-05-28 17:58:54 -0700

    Add pui buttons

--- app/assets/javascripts/react-components/views/home.jsx
+++ app/assets/javascripts/react-components/views/home.jsx
@@ -1,5 +1,7 @@
 /** @jsx React.DOM */
 var React = require('react');
+var DefaultButton = require('pui-react-buttons').DefaultButton;
+var PrimaryButton = require('pui-react-buttons').PrimaryButton;
 
 (function () {
   'use strict';
@@ -13,6 +15,8 @@ var React = require('react');
             <div className="container">
               <h1 className="title txt-c em-low mvn">Prototyping with React and Node</h1>
               <p className="h2 txt-c em-low mvn">You can do all sorts of fun things with this!</p>
+              <DefaultButton>Default Button</DefaultButton>
+              <PrimaryButton>Default Button</PrimaryButton>
             </div>
           </div>
           <hr className="mvn" />
